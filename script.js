bf791ef4836a72918cd0268a2eeb08d9d213f24f
IvanSlivenko <ivan8822@ulr.net>
2022-12-08 21:29:38 +0200

    etap-08-12-2022-21-29

--- script.js
+++ script.js
@@ -2619,39 +2619,39 @@
 //////////////////////////////////////////////////////////////////////
 //4/4
 
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName,makePizza,onOrderError) {
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName,makePizza,onOrderError) {
   
-    // for (const element of this.pizzas) {
-      console.log("//");
-      console.log(this.pizzas);
-      console.log(pizzaName);
-      // console.log(element);
+//     // for (const element of this.pizzas) {
+//       console.log("//");
+//       console.log(this.pizzas);
+//       console.log(pizzaName);
+//       // console.log(element);
       
-      if (this.pizzas.includes(pizzaName)) {
-      console.log("ok");
-      makePizza(pizzaName);
-      } else { 
-        console.log("trabl");
-      onOrderError(pizzaName);
-    }
-  // }
-},
-};
-// Change code above this line
+//       if (this.pizzas.includes(pizzaName)) {
+//       console.log("ok");
+//       makePizza(pizzaName);
+//       } else {
+//         console.log("trabl");
+//       onOrderError(pizzaName);
+//     }
+//   // }
+// },
+// };
+// // Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  console.log(`Your order is accepted. Cooking pizza ${pizzaName}.`);
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   console.log(`Your order is accepted. Cooking pizza ${pizzaName}.`);
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
 
-// Callback for onError
-function onOrderError(pizzaName) {
-  console.log(`Error! There is no pizza with a name  ${pizzaName} in the assortment. `);
-  return `Error! There is no pizza with a name  ${pizzaName} in the assortment. `;
-}
+// // Callback for onError
+// function onOrderError(pizzaName) {
+//   console.log(`Error! There is no pizza with a name  ${pizzaName} in the assortment. `);
+//   return `Error! There is no pizza with a name  ${pizzaName} in the assortment. `;
+// }
 
 // Method calls with callbacks
 // pizzaPalace.order("Smoked", makePizza, onOrderError);
@@ -2669,14 +2669,446 @@ function onOrderError(pizzaName) {
 // Виклик pizzaPalace.order("Vienna", makePizza, onOrderError)
 //повертає "Error! There is no pizza with a name Vienna in the assortment."
 ///////////////////////////////////////////////////////////////////////////////////////
+// var bar = "global";
 
+// function foo() {
+//   console.log(this.bar);
+// }
+// foo();
+// var obj1 = {
+//   bar: "obj1",
+//   foo: foo,
 
+// }
+// obj1.foo();
+// var obj2 = {
+//   bar:"obj2"
+// };
+// foo.call(obj2);
+// new foo();
+////////////////////////////////////////////////////////////
+// var foo = {
+//   a:42
+// }
 
+// var bar = Object.create(foo);
 
+// bar.b = "222";
+// console.log(bar.a);
+// console.log(bar.b);
+//////////////////////////////////////////////////
+// var name = "Ivan";
+// function identify() {
+// // console.log(this.name.toUpperCase());
+//   return this.name.toUpperCase();
+// };
+  
+// identify();
+// function speak() {
+//   var greting = "Hello, I'm " + identify.call(this)
+//   console.log(greting);
+// };
+// speak();
+// ////////////////////////////////////////////////////////////////
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+//   orderedItems.forEach(function(number,index){
+//     totalPrice=totalPrice+number;
+//   });
   
 
+//   // Change code above this line
+//   // console.log(totalPrice);
+//   return totalPrice;
+// }
+
+// calculateTotalPrice([12, 85, 37, 4])
+// //повертає 138
+// calculateTotalPrice([164, 48, 291])
+//   //повертає 503
+// calculateTotalPrice([412, 371, 94, 63, 176])
+//   //повертає 1116
+// ////////////////////////////////////////////////////////////////////////
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+//     numbers.forEach(function (number, index) {
+//       if (number > value) {
+         
+//       filteredNumbers.push(number);
+//     };
+//   });
+  
+//   // Change code above this line
+//   console.log(filteredNumbers);
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3)
+//   //повертає [4, 5]
+// filterArray([1, 2, 3, 4, 5], 4)
+//   //повертає [5]
+// filterArray([1, 2, 3, 4, 5], 5)
+//   //повертає []
+// filterArray([12, 24, 8, 41, 76], 38)
+//   //повертає [41, 76]
+// filterArray([12, 24, 8, 41, 76], 20)
+//   //повертає [24, 41, 76]
+////////////////////////////////////////////////////////////////////////////////
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
 
 
+// firstArray.forEach(function(element,index) {
+//   if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+// });
+    
+// console.log("//////////////////////////");
+//   console.log(firstArray);
+//   console.log(secondArray);
+//   console.log(commonElements);
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// getCommonElements([1, 2, 3], [2, 4])
+//   //повертає [2]
+// getCommonElements([1, 2, 3], [2, 1, 17, 19])
+//   //повертає [1, 2]
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])
+//   //повертає [12, 27, 3]
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])
+//   //повертає [10, 30, 40]
+// getCommonElements([1, 2, 3], [10, 20, 30])
+//   //повертає []
+////////////////////////////////////////////////////////////////////////////////
+//   var calculateTotalPrice=(quantity, pricePerItem)=> {
+//   // Change code above this line
+//     var result = quantity * pricePerItem;
+//     console.log(result);
+//     return result;
+// }
+// calculateTotalPrice(5, 100)
+//   //повертає 500
+// calculateTotalPrice(8, 60)
+//   //повертає 480
+// calculateTotalPrice(3, 400)
+//   //повертає 1200
+  /////////////////////////////////////////////////////////////////////////////////
+  // Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+// const calculateTotalPrice = (quantity, pricePerItem) =>
+//  quantity * pricePerItem;
+ 
+// // };
+// // Change code above this line
+// calculateTotalPrice(5, 100)
+//   //повертає 500
+// calculateTotalPrice(8, 60)
+//   //повертає 480
+// calculateTotalPrice(3, 400)
+//   //повертає 1200
+  ////////////////////////////////////////////////////////
+// Change code below this line
+// const calculateTotalPrice=(orderedItems)=> {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item,index)=> {
+//     totalPrice += item;
+//   });
+// console.log(totalPrice);
+//   return totalPrice;
+// }
+// // Change code above this line
+// calculateTotalPrice([12, 85, 37, 4])
+//   //повертає 138
+// calculateTotalPrice([164, 48, 291])
+//   //повертає 503
+// calculateTotalPrice([412, 371, 94, 63, 176])
+//   //повертає 1116
+  ///////////////////////////////////////////////////////////
+// const filterArray=(numbers, value)=> {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number,index)=> {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   console.log(filteredNumbers);
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3)
+//   //повертає [4, 5]
+// filterArray([1, 2, 3, 4, 5], 4)
+//   //повертає [5]
+// filterArray([1, 2, 3, 4, 5], 5)
+//   //повертає []
+// filterArray([12, 24, 8, 41, 76], 38)
+//   //повертає [41, 76]
+// filterArray([12, 24, 8, 41, 76], 20)
+//   //повертає [24, 41, 76]
+  //////////////////////////////////////////////////
+// // Change code below this line
+// const getCommonElements=(firstArray, secondArray)=> {
+//   const commonElements = [];
+
+//   firstArray.forEach((element,index)=> {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+// console.log(commonElements);
+//   return commonElements;
+// }
+// getCommonElements([1, 2, 3], [2, 4])
+//   //повертає [2]
+// getCommonElements([1, 2, 3], [2, 1, 17, 19])
+//   //повертає [1, 2]
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])
+//   //повертає [12, 27, 3]
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])
+//   //повертає [10, 30, 40]
+// getCommonElements([1, 2, 3], [10, 20, 30])
+//   //повертає []
+  /////////////////////////////////////////////////
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const NumbersTwo = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       // numbers[i] = numbers[i] + value;
+//       NumbersTwo.push(numbers[i] + value)
+//     }
+//     else {
+//       NumbersTwo.push(numbers[i]);
+//     }
+//   }
+//   // Change code above this line
+//   console.log("//////////////////////////");
+//   console.log(numbers);
+//   console.log(NumbersTwo);
+//   return NumbersTwo;
+// }
+// changeEven([1, 2, 3, 4, 5], 10)
+//   //повертає новий масив [1, 12, 3, 14, 5]
+// changeEven([2, 8, 3, 7, 4, 6], 10)
+//   //повертає новий масив [12, 18, 3, 7, 14, 16]
+// changeEven([17, 24, 68, 31, 42], 100)
+//   //повертає новий масив [17, 124, 168, 31, 142]
+// changeEven([44, 13, 81, 92, 36, 54], 100)
+//   //повертає новий масив [144, 13, 81, 192, 136, 154]
+  //////////////////////////////////////
+//   /////////////////////////////////////////////////////////////////////
+//   const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map(planeta=>planeta.length);
+// console.log(planetsLengths);
+
+// //Оголошена змінна planets
+// // Значення змінної planets - це масив ["Earth", "Mars", "Venus", "Jupiter"]
+// // Оголошена змінна planetsLengths
+// // Значення змінної planetsLengths - це масив [5, 4, 5, 7]
+// //////////////////////////////////////////////////////////////////////////////
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(element => element.title);
+// console.log(titles);
+
+// // Значення змінної titles - це масив ["The Last Kingdom",
+// //"Beside Still Waters", "The Dream of a Ridiculous Man",
+// //"Redder Than Blood", "Enemy of God"]
+//////////////////////////////////////////////////////////////////////
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap(element => element = element.genres);
+// console.log(genres);
+
+// Значення змінної genres - це масив [ "adventure", "history", "fiction", "horror", "mysticism" ]
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Change code below this line
+// const users=[
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+// const getUserEmails = users => {
+//     const arrayEmail =users.map(element=>element=element.email)
+// console.log(arrayEmail);
+//   };
+//   // Change code above this line
+
+// getUserEmails(users);
+
+// // Виклик функції із зазначеним масивом користувачів повертає масив
+// //["moorehensley@indexia.com", "sharlenebush@tubesys.com",
+// //"rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com",
+// //"blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+///////////////////////////////////////////////////////////////////////////////////
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+// const oddNumbers = numbers.filter(number => number % 2 >0 );;
+// console.log("///////////////////////////////");
+// console.log(numbers);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
 
 
+// // Оголошена змінна numbers
+// // Значення змінної numbers - це масив [17, 24, 82, 61, 36, 18, 47, 52, 73]
+// // Оголошена змінна evenNumbers
+// // Значення змінної evenNumbers - це масив [24, 82, 36, 18, 52]
+// // Оголошена змінна oddNumbers
+// // Значення змінної oddNumbers - це масив [17, 61, 47, 73]
+// // Для перебирання масиву numbers використаний метод filter()
+///////////////////////////////////////////////////////////////////////////////
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(element=>element.genres);
+// const uniqueGenres = allGenres.filter((genr, index, allGenres) => allGenres.indexOf(genr) === index);
+
+
+// console.log(allGenres);
+// console.log(uniqueGenres);
+// // Значення змінної allGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+// // Оголошена змінна uniqueGenres
+// // Значення змінної uniqueGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror"]
+// // Для обчислення значення змінної allGenders використаний метод flatMap()
+// // Для обчислення значення змінної uniqueGenres використаний метод filter()
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
