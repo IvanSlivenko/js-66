b0a9277c469edd8ed415b27c32aa91a621ae4da2
IvanSlivenko <ivan8822@ulr.net>
2022-12-16 21:58:23 +0200

    etap-16-12-2022-21-58

--- script.js
+++ script.js
@@ -5224,7 +5224,7 @@
 // console.log(User.Roles);
 // //////////////////////////////////////////////////////////////////////////
 // class Car {
-//   static MAX_PRICE = 50000; 
+//   static MAX_PRICE = 50000;
 //   // Change code below this line
 //   #price;
 
@@ -5238,7 +5238,7 @@
 
 //   set price(newPrice) {
 //     if (newPrice < Car.MAX_PRICE)
-//     { 
+//     {
 //        this.#price = newPrice;
       
 //     }
@@ -5256,5 +5256,137 @@
 // audi.price = 51000;
 // console.log(audi.price); // 49000
 // ///////////////////////////////////////////////////////////////
+// class User {
+//   static #takenEmails = [];
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+//   #email;
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
 
+// const mango = new User({ email: "mango@mail.com" });
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// class Car {
+  
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       // console.log("Success! Price is within acceptable limits");
+//       return "Error! Price exceeds the maximum";
+//     }
+//     // console.log("Error! Price exceeds the maximum");
+//     return "Success! Price is within acceptable limits";
+//   }
+
+// }
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+// //////////////////////////////////////////////////////////////////////////////////////////
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+//   get email() {
+//     return this.email;
+//    }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+
+// }
+
+// class ContentEditor extends User {
+
+// }
+
+
+// const editor = new ContentEditor("mango@mail.com");
+// console.log(editor);
+// /////////////////////////////////////////////////////////
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor(AccessLevel) {
+//     this.AccessLevel = AccessLevel;
+     
+//   }
+// }
+
+// console.log(Admin.AccessLevel.BASIC);
+// console.log(Admin.AccessLevel.SUPERUSER);
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  constructor({ email, accessLevel }) { 
+    super(email);
+    this.accessLevel = accessLevel;
+    
+  }
+
+  // Change code above this line
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+///////////////////////////////////////////////////////////////////////////
 
