b47c913c34734d45eb3ca396082ef2edd0a7794e
IvanSlivenko <ivan8822@ulr.net>
2023-01-08 21:46:19 +0200

    etap-08-01-2023-21-46

--- js/03-tags.js
+++ js/03-tags.js
@@ -1,19 +1,60 @@
 
 
+// const tagsContainer = document.querySelector('.js-tags');
+// let selectedTag = null;
+ 
+// tagsContainer.addEventListener('click', onTagsconteinerclick);
+
+// /////////////////////////////////////////////////////////////////////////////
+// function onTagsconteinerclick(e) {
+//     if (e.target.nodeName!=='BUTTON') {
+//         return
+//     }
+//     const currentActiveBtn = document.querySelector('.tags__btn--active');
+    
+//  if (currentActiveBtn) {
+//     currentActiveBtn.classList.remove('tags__btn--active');
+//  }
+//     // currentActiveBtn?.classList.remove('tags__btn--active');
+    
+//     const nextActiveBtn = e.target;
+//     nextActiveBtn.classList.add('tags__btn--active');
+//     selectedTag = nextActiveBtn.dataset.value;
+//     console.log(selectedTag);
+// }
+    /////////////////////////////////////////////////////////////////////////
+
+
 const tagsContainer = document.querySelector('.js-tags');
-
-
+const selectedTags = new Set();
+ 
 tagsContainer.addEventListener('click', onTagsconteinerclick);
 
 function onTagsconteinerclick(e) {
     if (e.target.nodeName!=='BUTTON') {
         return
     }
-    const currentActiveBtn = document.querySelector('.tags__btn--active');
-    console.log(currentActiveBtn);
-    // currentActiveBtn.classList.toggle('tags__btn--active');
-    // console.log(e.target);
-    e.target.classList.add('tags__btn--active');
+    const btn = e.target;
+    const tag = btn.dataset.value;
+    const isActive = btn.classList.contains('tags__btn--active');
+    
+    console.log(btn.classList.contains('tags__btn--active')); 
+
+    
+    // const btnActiv = document.querySelector('.tags__btn--active');
+    if (isActive) {
+        selectedTags.delete(tag);
+          }
+   
+    else { 
+        selectedTags.add(tag);    
+    }
+     
+  btn.classList.toggle('tags__btn--active');  
+  console.log(selectedTags);
 }
 
-//time end ===33:00
+
+//https://www.youtube.com/watch?v=pNxBoxbnTVA
+//time end ===55:21
+
